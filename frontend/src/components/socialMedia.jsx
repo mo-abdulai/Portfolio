a4df91ccac6968aa6684bdff676696ed19489355
Mohammed Abdulai <nurud43@gmail.com>
2024-03-21 01:18:49 -0400

    skills and experience section complete

--- frontend/src/components/socialMedia.jsx
+++ frontend/src/components/socialMedia.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import {BsTwitter, BsInstagram } from 'react-icons/bs'
-import { FaFacebookF } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
 const socialMedia = () => {
   return (
     <div className='app__social'>
+       <div>
+        <a href='https://www.linkedin.com/in/mo-abdulai/'>
+       <FaLinkedin/>
+        </a>
+      </div>
       <div>
+       <a href='https://twitter.com/mo_abdulai?s=21'>
        <BsTwitter/>
+       </a>
       </div>
       <div>
+        <a href='#'>
        <BsInstagram/>
-      </div>
-      <div>
-       <FaFacebookF/>
+        </a>
       </div>
     </div>
   )
